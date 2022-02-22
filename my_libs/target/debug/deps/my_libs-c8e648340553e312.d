3408529f92f84e470b7854dcfd79bba7217f4c5e
seal9055 <seal9055@gmail.com>
2022-02-22 00:45:10 -0500

    created simple library to maintain sorted vector

--- my_libs/target/debug/deps/my_libs-c8e648340553e312.d
+++
@@ -1,7 +0,0 @@
-/home/seal/github/local_crates/my_libs/target/debug/deps/my_libs-c8e648340553e312: src/lib.rs src/sorted_vec.rs src/elf_parser.rs
-
-/home/seal/github/local_crates/my_libs/target/debug/deps/my_libs-c8e648340553e312.d: src/lib.rs src/sorted_vec.rs src/elf_parser.rs
-
-src/lib.rs:
-src/sorted_vec.rs:
-src/elf_parser.rs:
--- my_libs/target/debug/deps/my_libs-e5ad55d9d9b059ad.d
+++
@@ -1,9 +0,0 @@
-/home/seal/github/local_crates/my_libs/target/debug/deps/my_libs-e5ad55d9d9b059ad.rmeta: src/lib.rs src/sorted_vec.rs src/elf_parser.rs
-
-/home/seal/github/local_crates/my_libs/target/debug/deps/libmy_libs-e5ad55d9d9b059ad.rlib: src/lib.rs src/sorted_vec.rs src/elf_parser.rs
-
-/home/seal/github/local_crates/my_libs/target/debug/deps/my_libs-e5ad55d9d9b059ad.d: src/lib.rs src/sorted_vec.rs src/elf_parser.rs
-
-src/lib.rs:
-src/sorted_vec.rs:
-src/elf_parser.rs:
