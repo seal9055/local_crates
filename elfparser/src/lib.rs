941be9225e4a466bfbbc109668b567a46d6d1a7e
seal9055 <seal9055@gmail.com>
2022-07-01 13:10:28 -0400

    added some 32-bit elf support

--- elfparser/src/lib.rs
+++ elfparser/src/lib.rs
@@ -1,6 +1,12 @@
 use byteorder::{LittleEndian, ReadBytesExt};
 
 pub const ELFMAGIC:     u32 = 0x464c457f;
+pub const BITSZ32:      u8  = 0x1;
+pub const BITSZ64:      u8  = 0x2;
+pub const LITTLEENDIAN: u8  = 0x1;
+pub const TYPEEXEC:     u16 = 0x2;
+pub const LOADSEGMENT:  u32 = 0x1;
+pub const RISCV:        u16 = 0xf3;
 
 #[derive(Debug, Clone)]
 pub struct ELF {
@@ -11,8 +17,8 @@ pub struct ELF {
 impl ELF {
     pub fn parse_elf(buf: &[u8]) -> Self {
         let header = Header::new(buf).expect("Failed to parse elf header");
-        let program_headers = ProgramHeader::parse_headers(&header, buf).unwrap();
         assert_eq!(header.magic, ELFMAGIC, "Not an elf");
+        let program_headers = ProgramHeader::parse_headers(&header, buf).unwrap();
         Self {
             header,
             program_headers,
@@ -47,9 +53,13 @@ pub struct Header {
 impl Header {
     pub fn new(mut binary: &[u8]) -> Option<Self> {
         if binary.len() < std::mem::size_of::<Header>() { return None; }
+        let bitsize;
         Some(Header {
             magic            : binary.read_u32::<LittleEndian>().unwrap(),
-            bitsize          : binary.read_u8::<>().unwrap(),
+            bitsize          : {
+                bitsize = binary.read_u8::<>().unwrap();
+                bitsize
+            },
             endian           : binary.read_u8::<>().unwrap(),
             ident_abi_version: binary.read_u8::<>().unwrap(),
             target_platform  : binary.read_u8::<>().unwrap(),
@@ -58,9 +68,27 @@ impl Header {
             o_type           : binary.read_u16::<LittleEndian>().unwrap(),
             machine          : binary.read_u16::<LittleEndian>().unwrap(),
             version          : binary.read_u32::<LittleEndian>().unwrap(),
-            entry_addr       : binary.read_u64::<LittleEndian>().unwrap() as usize,
-            phoff            : binary.read_u64::<LittleEndian>().unwrap() as usize,
-            shoff            : binary.read_u64::<LittleEndian>().unwrap() as usize,
+            entry_addr       : {
+                match bitsize {
+                    BITSZ32 => binary.read_u32::<LittleEndian>().unwrap() as usize,
+                    BITSZ64 => binary.read_u64::<LittleEndian>().unwrap() as usize,
+                    _ => unreachable!(),
+                }
+            },
+            phoff       : {
+                match bitsize {
+                    BITSZ32 => binary.read_u32::<LittleEndian>().unwrap() as usize,
+                    BITSZ64 => binary.read_u64::<LittleEndian>().unwrap() as usize,
+                    _ => unreachable!(),
+                }
+            },
+            shoff       : {
+                match bitsize {
+                    BITSZ32 => binary.read_u32::<LittleEndian>().unwrap() as usize,
+                    BITSZ64 => binary.read_u64::<LittleEndian>().unwrap() as usize,
+                    _ => unreachable!(),
+                }
+            },
             flags            : binary.read_u32::<LittleEndian>().unwrap(),
             ehsize           : binary.read_u16::<LittleEndian>().unwrap(),
             phentsize        : binary.read_u16::<LittleEndian>().unwrap(),
@@ -85,7 +113,21 @@ pub struct ProgramHeader {
 }
 
 impl ProgramHeader {
-    pub fn new(mut binary: &[u8]) -> Option<Self> {
+    pub fn new_32(mut binary: &[u8]) -> Option<Self> {
+        if binary.len() < std::mem::size_of::<ProgramHeader>() { return None; }
+        Some(ProgramHeader {
+            seg_type: binary.read_u32::<LittleEndian>().unwrap(),
+            offset  : binary.read_u32::<LittleEndian>().unwrap() as usize,
+            vaddr   : binary.read_u32::<LittleEndian>().unwrap() as usize,
+            paddr   : binary.read_u32::<LittleEndian>().unwrap() as usize,
+            filesz  : binary.read_u32::<LittleEndian>().unwrap() as usize,
+            memsz   : binary.read_u32::<LittleEndian>().unwrap() as usize,
+            flags   : binary.read_u32::<LittleEndian>().unwrap(),
+            align   : binary.read_u32::<LittleEndian>().unwrap() as usize,
+        })
+    }
+
+    pub fn new_64(mut binary: &[u8]) -> Option<Self> {
         if binary.len() < std::mem::size_of::<ProgramHeader>() { return None; }
         Some(ProgramHeader {
             seg_type: binary.read_u32::<LittleEndian>().unwrap(),
@@ -104,11 +146,14 @@ impl ProgramHeader {
         let mut program_headers = Vec::new();
         for _ in 0..elf_hdr.phnum {
             offset += elf_hdr.phentsize as usize;
-            let program_hdr = ProgramHeader::new(&buf[offset..])?;
+            let program_hdr = match elf_hdr.bitsize {
+                BITSZ32 => ProgramHeader::new_32(&buf[offset..])?,
+                BITSZ64 => ProgramHeader::new_64(&buf[offset..])?,
+                _ => unreachable!(),
+            };
 
             program_headers.push(program_hdr);
         }
-
         Some(program_headers)
     }
 }
